d270b5847e5a1909dbc67e53d776b1e313814b42
philicious <the.cypher@gmail.com>
2015-02-11 17:32:41 +0100

    cloud CI test with chef-zero

---
+++ client.rb
@@ -0,0 +1,11 @@
+node_name "ci-ubuntu"
+#cookbook_path ["/tmp/kitchen/cookbooks", "/tmp/kitchen/site-cookbooks"]
+#data_bag_path "/tmp/kitchen/data_bags"
+#environment_path "/tmp/kitchen/environments"
+#node_path "/tmp/kitchen/nodes"
+#role_path "/tmp/kitchen/roles"
+#client_path "/tmp/kitchen/clients"
+#user_path "/tmp/kitchen/users"
+validation_key "./dummy-validation.pem"
+#client_key "./client.pem"
+chef_server_url "http://127.0.0.1:8889"
