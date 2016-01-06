7071499a231b4fb4bdff9dd84915094a16d87790
Denis Looby <denis.looby@hpe.com>
2016-01-06 19:24:02 +0000

    custom resource fixes

--- recipes/first_user.rb
+++ recipes/first_user.rb
@@ -1,26 +1,26 @@
 
-spacewalk-server_first-user 'admin' do
-  password "admin1234"
-  first_name "Joe"
-  last_name "Blogs"
+spacewalk_admin 'admin' do
+  password 'admin1234'
+  first_name 'Joe'
+  last_name 'Blogs'
   db_user node['spacewalk']['server']['db']['user']
   db_password node['spacewalk']['server']['db']['password']
   db_schema node['spacewalk']['server']['db']['name']
 end
 
-psqlscript = Chef::Config[:file_cache_path] + '/first_user.psql'
-
- template psqlscript do
-   source 'first_user.psql.erb'
-   variables ({
-     :first_user_name => 'admin',
-     :first_user_password => '$5$Q2fxUTqBPoynaN9M$pptnmFj10UW3v2mNCfHJX5k4R9fmQd6kY0bX8Xrtb/6',
-     :first_user_prefix => 'Mr.',
-     :first_user_first_name => 'Joe',
-     :first_user_last_name => 'Blogs'
-     })
- end
-
- execute 'psql_script' do
-   command "export PGPASSWORD=#{node['spacewalk']['server']['db']['password']}; psql -1 -f " + psqlscript +" #{node['spacewalk']['server']['db']['name']} #{node['spacewalk']['server']['db']['user']}"
- end
+# psqlscript = Chef::Config[:file_cache_path] + '/first_user.psql'
+#
+#  template psqlscript do
+#    source 'first_user.psql.erb'
+#    variables ({
+#      :first_user_name => 'admin',
+#      :first_user_password => '$5$Q2fxUTqBPoynaN9M$pptnmFj10UW3v2mNCfHJX5k4R9fmQd6kY0bX8Xrtb/6',
+#      :first_user_prefix => 'Mr.',
+#      :first_user_first_name => 'Joe',
+#      :first_user_last_name => 'Blogs'
+#      })
+#  end
+#
+#  execute 'psql_script' do
+#    command "export PGPASSWORD=#{node['spacewalk']['server']['db']['password']}; psql -1 -f " + psqlscript +" #{node['spacewalk']['server']['db']['name']} #{node['spacewalk']['server']['db']['user']}"
+#  end
--- resources/first-user.rb
+++ resources/first-user.rb
@@ -1,19 +1,20 @@
 
+resource_name :spacewalk_admin
+
 property :user_name, String, name_property: true
 property :password, String, required: true
 property :salt, String, default: 'Q2fxUTqBPoynaN9M'
 property :prefix, String, default:  'Mr.'
 property :first_name, String, required: true
 property :last_name, String, required: true
-
 property :db_user, String, required: true
 property :db_password, String, required: true
 property :db_schema, String, required: true
 
 load_current_value do
   cmd = "export PGPASSWORD=#{db_password}; VALUE=$(psql -d #{db_schema} -U #{db_user} -qtA -c 'select exists(select 1  from web_contact);');echo $VALUE"
-  user_exists = %x( cmd )
-  puts user_exits
+  user_exists = %x( #{cmd} )
+  puts user_exists
 end
 
 action :start do
