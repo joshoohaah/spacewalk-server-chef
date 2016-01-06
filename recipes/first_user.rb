409af62a3439ca418a0dd2a40f181f64ba049a78
Denis Looby <denis.looby@hpe.com>
2016-01-06 11:51:52 +0000

    First attempt to hack in first spacewalk user

---
+++ recipes/first_user.rb
@@ -0,0 +1,16 @@
+psqlscript = Chef::Config[:file_cache_path] + '/first_user.psql'
+
+ template psqlscript do
+   source 'first_user.psql.erb'
+   variables ({
+     :first_user_name => 'admin',
+     :first_user_password => '$5$Q2fxUTqBPoynaN9M$pptnmFj10UW3v2mNCfHJX5k4R9fmQd6kY0bX8Xrtb/6',
+     :first_user_prefix => 'Mr.',
+     :first_user_first_name => 'Joe',
+     :first_user_last_name => 'Blogs'
+     })
+ end
+
+ execute 'psql_script' do
+   command "export PGPASSWORD=#{node['spacewalk']['server']['db']['password']}; psql -1 -f " + psqlscript +" #{node['spacewalk']['server']['db']['name']} #{node['spacewalk']['server']['db']['user']}"
+ end
