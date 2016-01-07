6c96c0bc4908be023e6045f8475cf38440181617
Denis Looby <denis.looby@hpe.com>
2016-01-07 18:24:36 +0000

    Generate a default salt and load_current_value changes

--- resources/first-user.rb
+++ resources/first-user.rb
@@ -1,16 +1,16 @@
+require 'securerandom'
 
 resource_name :spacewalk_admin
 
 property :login, String, name_property: true
 property :password, String, required: true
-property :salt, String, default: 'Q2fxUTqBPoynaN9M'
+property :salt, String, default: lazy{ SecureRandom.urlsafe_base64(12) }#'Q2fxUTqBPoynaN9M'
 property :prefix, String, required: true
 property :first_name, String, required: true
 property :last_name, String, required: true
 property :db_user, String, required: true
 property :db_password, String, required: true
 property :db_schema, String, required: true
-property :user_exists, String, default: "t" # Used for the idempotent check should not be used in recipes
 
 #
 # For the idempotent check we are only looking to see if there is a row in web_contact.
@@ -21,21 +21,22 @@ load_current_value do |new_resource|
   # It must be done before anything else too, cheat and just use values from the new resource and we have new_resource available
   # new_resource is being passed in because we need the db user etc to run the query check
 
-  login = new_resource.login
-  password = new_resource.password
-  prefix = new_resource.prefix
-  first_name = new_resource.first_name
-  last_name = new_resource.last_name
-  db_user = new_resource.db_user
-  db_password = new_resource.db_password
-  db_schema = new_resource.db_schema
+  self.login = new_resource.login
+  self.password = new_resource.password
+  self.prefix = new_resource.prefix
+  self.first_name = new_resource.first_name
+  self.last_name = new_resource.last_name
+  self.db_user = new_resource.db_user
+  self.db_password = new_resource.db_password
+  self.db_schema = new_resource.db_schema
 
   cmd = "export PGPASSWORD=#{new_resource.db_password}; VALUE=$(psql -d #{new_resource.db_schema} -U #{new_resource.db_user} -qtA -c 'select exists(select 1  from web_contact);');echo $VALUE"
   user_exists = %x( #{cmd} )
+  raise Chef::Exceptions::CurrentValueDoesNotExist if "f".eql? user_exists[0]
 end
 
 action :start do
-  converge_if_changed :user_exists do
+  converge_if_changed do
     psqlscript = Chef::Config[:file_cache_path] + '/first_user.psql'
 
     # Web UI appears to store a salted sha256 hash of the password
